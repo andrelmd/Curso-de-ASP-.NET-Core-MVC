f950cae94375762372a5fbb7bb4b00dd84bb5fe6
André Luís Maldonado Daniel <almaldonado@viptech.com.br>
2024-03-12 19:07:35 -0300

    aidicona item do carrinho

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,10 +11,16 @@ namespace LanchesMac.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+            NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id
+                ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,9 @@
-﻿namespace LanchesMac;
+﻿using LanchesMac.Context;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac;
 public class Startup
 {
     public Startup(IConfiguration configuration)
@@ -11,6 +16,12 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddTransient<ILancheRepository, LancheRepository>();
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+
         services.AddControllersWithViews();
     }
 
